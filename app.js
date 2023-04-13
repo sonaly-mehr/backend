40d212e7234aa5affd0a1040e52cc9e855437955
sonaly-mehr <sonaly.mehr@gmail.com>
2023-04-13 21:59:37 +0600

    test

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
-    origin:"http://localhost:3000",
+    origin:"https://ecommerce-shop-webapp.netlify.app",
     credentials: true,
   })
 );
